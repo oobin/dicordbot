c92ac10877833fcdb69d9d952db9fd58b35f7004
Robin <robin@klett.co>
2023-11-24 14:25:52 +0100

    added chatgpt to bot

--- bot.py
+++ bot.py
@@ -1,22 +1,43 @@
 import os
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands
+import openai
 
 
 def main():
     load_dotenv()
-    client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-    
-    @client.event
+    bot = discord.Client(command_prefix="!", intents=discord.Intents.all())
+
+    @bot.event
     async def on_ready():
-        print(f"{client.user.name} has connected to Discord.")
+        print(f"{bot.user.name} has connected to Discord.")
 
-    @client.command()
-    async def ping(ctx):
-        await ctx.send("Pong!")
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
 
-    client.run(os.getenv("DISCORD_TOKEN"))
+        if bot.user in message.mentions:
+            response = openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": message.content}],
+                max_tokens=512,
+                temperature=0.2,
+                frequency_penalty=0,
+                presence_penalty=0,
+            )
 
-if __name__ == '__main__':
+            content = response.choices[0].message.content
+            chunk_size = 2000
+            chunks = [
+                content[i : i + chunk_size] for i in range(0, len(content), chunk_size)
+            ]
+
+            for chunk in chunks:
+                await message.channel.send(chunk)
+
+    bot.run(os.getenv("DISCORD_TOKEN"))
+
+
+if __name__ == "__main__":
     main()
