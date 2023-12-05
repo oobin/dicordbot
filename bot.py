258a39e2aad420dfab3e2acb8fd6a8132e5350f3
Robin <robin@klett.co>
2023-12-05 19:18:04 +0100

    small refactor bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,13 @@ from openai import OpenAI
 
 def main():
     load_dotenv()
-    bot = discord.Client(command_prefix="!", intents=discord.Intents.all())
+    intents = discord.Intents.all()
+    bot = discord.Client(command_prefix="!", intents=intents)
     ai_client = OpenAI()
 
     @bot.event
     async def on_ready():
-        print(f"{bot.user.name} has connected to Discord.")
+        print(f"{bot.user} has connected to Discord.")  
 
     @bot.event
     async def on_message(message):
@@ -20,24 +21,17 @@ def main():
 
         if bot.user in message.mentions:
             response = ai_client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": message.content}],
-                max_tokens=256,
-                temperature=0.5,
-                frequency_penalty=0,
-                presence_penalty=0,
-            )
+                    model="gpt-3.5-turbo",
+                    messages=[{"role": "user", "content": message.content}],
+                    max_tokens=256,
+                    temperature=0.5,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    )
 
-            content = response.choices[0].message.content
-            chunk_size = 2000
-            chunks = [
-                content[i : i + chunk_size] for i in range(0, len(content), chunk_size)
-            ]
+            await message.channel.send(response.choices[0].message.content)
 
-            for chunk in chunks:
-                await message.channel.send(chunk)
-
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    bot.run(os.getenv("DISCORD_TOKEN"))  # type: ignore
 
 
 if __name__ == "__main__":
