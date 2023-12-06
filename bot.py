29a90fcde06540dc44e0af085900f91f43b693dc
Robin <robin@klett.co>
2023-12-06 22:01:22 +0100

    resolved merge conflicts

--- bot.py
+++ bot.py
@@ -28,10 +28,6 @@ def main():
     intents = discord.Intents.all()
     bot = discord.Client(command_prefix="!", intents=intents)
     ai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-<<<<<<< HEAD
-    # conversation = [{"role": "system", "content": ""}]
-=======
->>>>>>> dev
     conversation = [{"role": "system", "content": INSTRUCTIONS}]
 
     @bot.event
