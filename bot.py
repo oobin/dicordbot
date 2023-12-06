c1157f0aa06a7a02951db5220b6ca08399733188
Robin <robin@klett.co>
2023-12-06 17:50:53 +0100

    refactor bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,21 @@ import discord
 from dotenv import load_dotenv
 from openai import OpenAI
 
+# ChatGPT options
+MAX_TOKENS = 512
+TEMPERATURE = 0.5
+FREQUENCY_PENALTY = 0
+PRESENCE_PENALTY = 0
+
+# limits how long the prompt can be
+MAX_CONTEXT_QUESTIONS = 5
+
 
 def main():
     load_dotenv()
     intents = discord.Intents.all()
     bot = discord.Client(command_prefix="!", intents=intents)
-    ai_client = OpenAI()
-    dev_mode = os.getenv("DEV_MODE")
-
+    ai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     conversation = []
 
     @bot.event
@@ -25,20 +32,20 @@ def main():
 
         if bot.user in message.mentions:
             async with message.channel.typing():
-                if len(conversation) > 4:
+                if len(conversation) >= MAX_CONTEXT_QUESTIONS:
                     conversation.pop(0)
                 conversation.append({"role": "user", "content": message.content})
                 response = ai_client.chat.completions.create(
                     model="gpt-3.5-turbo",
                     messages=conversation,
-                    max_tokens=512,
-                    temperature=0.5,
-                    frequency_penalty=0,
-                    presence_penalty=0,
+                    max_tokens=MAX_TOKENS,
+                    temperature=TEMPERATURE,
+                    frequency_penalty=FREQUENCY_PENALTY,
+                    presence_penalty=PRESENCE_PENALTY,
                 )
 
                 await message.channel.send(response.choices[0].message.content)
-                if len(conversation) > 4:
+                if len(conversation) >= MAX_CONTEXT_QUESTIONS:
                     conversation.pop(0)
                 conversation.append(
                     {
@@ -47,7 +54,7 @@ def main():
                     }
                 )
 
-    if dev_mode:
+    if os.getenv("DEV_MODE"):
         bot.run(os.getenv("DISCORD_DEV_TOKEN"))  # type: ignore
     else:
         bot.run(os.getenv("DISCORD_TOKEN"))  # type: ignore
